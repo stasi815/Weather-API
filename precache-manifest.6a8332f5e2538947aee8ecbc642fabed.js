ce003e9230c3073e0ed5febe65b1826d3ed1ee15
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-17 11:11:58 -0700

    Updates

---
+++ precache-manifest.6a8332f5e2538947aee8ecbc642fabed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5722e7be79f90d1276907bf85ec6b03d",
+    "url": "/Weather-API/index.html"
+  },
+  {
+    "revision": "db1049d41f8add9aee00",
+    "url": "/Weather-API/static/css/main.a20de9e7.chunk.css"
+  },
+  {
+    "revision": "8a6bf7ccecbbff4619e0",
+    "url": "/Weather-API/static/js/2.10d4690c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-API/static/js/2.10d4690c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1049d41f8add9aee00",
+    "url": "/Weather-API/static/js/main.8917fbb0.chunk.js"
+  },
+  {
+    "revision": "e770e7b25a81c6d6df7e",
+    "url": "/Weather-API/static/js/runtime-main.6d671614.js"
+  }
+]);
